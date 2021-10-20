c9f94b2a8ac69db2223ec8c2a5181253970e1b25
Marina Aoki <marina.aoki@web.de>
2021-10-20 21:14:04 +0200

    Added comment for clarification

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from __future__      import print_function
 from detect          import risk_notification
-from helper          import save_video
 
 if __name__ == '__main__':
+    # Replace this line with the path to the data.
     source = "/file/to/data/source"
     disclose_all = False
     threshold = 0.3
